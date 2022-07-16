6f96e0b29d5198ad4ec4fc690a8a460703665156
dipaso50 <dipaso50@gmail.com>
2022-07-16 16:48:33 +0200

    small change

--- default/keymap.c
+++ default/keymap.c
@@ -47,13 +47,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //----------------------------------                                         -------------------------------------
     KC_ESC , KC_1, KC_2, KC_3, KC_4, KC_5,                                       KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
     //----------------------------------                                         -------------------------------------
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                        KC_Y, KC_U, KC_I, KC_O, KC_P, MT(KC_RCTRL,KC_DEL),
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DEL,
     //----------------------------------                                         -------------------------------------
-    KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                                       KC_H, KC_J, KC_K, KC_L, KC_SCLN, MT(KC_RALT,KC_HOME),
+    KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                                       KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_HOME,
     //----------------------------------                                         -------------------------------------
-    KC_LCTRL, KC_Z, KC_X, KC_C, KC_V, KC_B,                ES_QUES, ES_EXLM,      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT(KC_RSFT,KC_END),
+    KC_LCTRL, KC_Z, KC_X, KC_C, KC_V, KC_B,                ES_QUES, ES_EXLM,      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,KC_END,
     //----------------------------------                            -------------------------------------
-                KC_LALT, _______, KC_LGUI, LT(_RAISE, KC_SPC), LT(_LOWER, KC_ENT), TD(TD_TILDE), TD(TD_QUOTES), KC_RALT),
+                KC_LALT, _______, KC_LGUI, LT(_RAISE, KC_SPC), LT(_LOWER, KC_ENT), TD(TD_TILDE), TD(TD_QUOTES), KC_RSFT),
 
 
 
