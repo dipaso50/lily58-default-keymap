2c5127b91ba38a548c051c2b0f45c763f4bbc372
dipaso50 <dipaso50@gmail.com>
2022-07-16 16:29:06 +0200

    stable layout

--- default/keymap.c
+++ default/keymap.c
@@ -3,13 +3,17 @@
 
 enum layer_number {
     _QWERTY = 0,
-    _LOWER,
-    _RAISE,
-    _SIMBOLS, 
+    _LOWER, 
+    _RAISE, 
+    _ADJUST,
 };
 
 enum {
-    TD_PARENTESIS_CAPS,
+    //TD_PARENTESIS_CAPS,
+    TD_QUOTES,
+    TD_TILDE,
+    TD_SLASH,
+    TD_COLONS,
 };
 
 void dance_one(qk_tap_dance_state_t *state, void *user_data) {
@@ -26,110 +30,75 @@ void dance_one_reset(qk_tap_dance_state_t *state, void *user_data) {
         unregister_code16(ES_LBRC);
 }
 
-/**qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for ( twice por {
-    [TD_PARENTESIS_CAPS] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_one, dance_one_reset),
-};**/
+
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for ( twice por {
-    [TD_PARENTESIS_CAPS] = ACTION_TAP_DANCE_DOUBLE(ES_LPRN, ES_LBRC),
+    // Tap once for " twice por '
+    [TD_QUOTES] = ACTION_TAP_DANCE_DOUBLE(ES_QUOT, ES_DQUO),
+    // Tap once for ' twice por `
+    [TD_TILDE] = ACTION_TAP_DANCE_DOUBLE(ES_ACUT, ES_GRV),
+    [TD_SLASH] = ACTION_TAP_DANCE_DOUBLE(ES_SLSH, ES_BSLS),
+    [TD_COLONS] = ACTION_TAP_DANCE_DOUBLE(ES_SCLN, ES_COLN),
 };
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
-
 [_QWERTY] = LAYOUT(
+    //----------------------------------                                         -------------------------------------
+    KC_ESC , KC_1, KC_2, KC_3, KC_4, KC_5,                                       KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+    //----------------------------------                                         -------------------------------------
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                        KC_Y, KC_U, KC_I, KC_O, KC_P, MT(KC_RCTRL,KC_DEL),
+    //----------------------------------                                         -------------------------------------
+    KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                                       KC_H, KC_J, KC_K, KC_L, KC_SCLN, MT(KC_RALT,KC_HOME),
+    //----------------------------------                                         -------------------------------------
+    KC_LCTRL, KC_Z, KC_X, KC_C, KC_V, KC_B,                ES_QUES, ES_EXLM,      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT(KC_RSFT,KC_END),
     //----------------------------------                            -------------------------------------
-    MT(KC_LSFT, KC_ESC), KC_1, KC_2, KC_3, KC_4, KC_5,              KC_6, KC_7, KC_8, KC_9, KC_0, MT(KC_RSFT,KC_BSPC),
-    //----------------------------------                            -------------------------------------
-    MT(KC_LCTRL,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T,              KC_Y, KC_U, KC_I, KC_O, KC_P, MT(KC_RCTRL,KC_DEL),
-    //----------------------------------                            -------------------------------------
-    MT(KC_LALT,ES_SCLN), KC_A, KC_S, KC_D, KC_F, KC_G,                          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_HOME,
-    //----------------------------------                            -------------------------------------
-    ES_COLN, KC_Z, KC_X, KC_C, KC_V, KC_B,   ES_QUES, ES_EXLM,     KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_END,
-    //----------------------------------                            -------------------------------------
-    _______, _______, KC_LGUI, LT(_LOWER, KC_SPC), LT(_SIMBOLS, KC_ENT), LT(_RAISE, ES_ACUT), ES_SLSH, KC_RALT),
- 
+                KC_LALT, _______, KC_LGUI, LT(_RAISE, KC_SPC), LT(_LOWER, KC_ENT), TD(TD_TILDE), TD(TD_QUOTES), KC_RALT),
+
+
 
 [_LOWER] = LAYOUT(
-    //----------------------------------                            -------------------------------------
-    KC_F11, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                      KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F12,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______,           ES_ASTR, ES_PIPE, _______, _______, KC_PSCR, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______,           KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______,           ES_BSLS, ES_SLSH, ES_QUOT, ES_GRV, ES_ACUT, _______, _______, _______,
-    //----------------------------------                            -------------------------------------
+    //----------------------------------                                         -------------------------------------
+    KC_F11, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F12,
+    //----------------------------------                                         -------------------------------------
+    _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, KC_PSCR, _______,
+    //----------------------------------                                         -------------------------------------
+    _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,
+    //----------------------------------                                         -------------------------------------
+    _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______, _______, _______,
+    //----------------------------------                                         -------------------------------------
     _______, _______, _______, _______, _______, _______, _______, _______),
-    //----------------------------------                            -------------------------------------
- 
-[_RAISE] = LAYOUT(
-    //----------------------------------                            -------------------------------------
-    _______, KC_1, KC_2, KC_3, KC_4, KC_5,                          KC_6, KC_7, KC_8, KC_9, KC_0, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, ES_MINS, ES_PLUS, ES_EQL,            ES_LPRN, ES_RPRN, _______, _______, _______, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, ES_LABK, ES_RABK,           ES_LCBR, ES_RCBR, _______, _______, _______, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______, _______,  ES_SCLN, ES_LBRC, ES_RBRC, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+    //----------------------------------                                         -------------------------------------
 
- 
-[_SIMBOLS] = LAYOUT(
-    //----------------------------------                            -------------------------------------
-    _______, ES_PIPE, ES_AT, ES_HASH, ES_DLR, ES_PERC,              ES_AMPR, ES_SLSH, ES_LPRN, ES_RPRN, ES_EQL, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______,           ES_ASTR, ES_CIRC, _______, _______, KC_PSCR, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______,
-    //----------------------------------                            -------------------------------------
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+[_RAISE] = LAYOUT(
+    //----------------------------------                                         -------------------------------------
+    _______, ES_PIPE, ES_AT, ES_HASH, ES_DLR, ES_PERC,                           ES_AMPR, ES_SLSH, ES_LPRN, ES_RPRN, ES_EQL, _______,
+    //----------------------------------                                         -------------------------------------
+    _______, _______, ES_LPRN, ES_RPRN, ES_LABK, ES_RABK,                        ES_ASTR, ES_TILD, ES_CIRC, _______, KC_PSCR, _______,
+    //----------------------------------                                         -------------------------------------
+    _______, _______, ES_LCBR, ES_RCBR, ES_MINS, ES_PLUS,                        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
+    //----------------------------------                                         -------------------------------------
+    _______, _______, ES_LBRC, ES_RBRC, _______, _______,   _______,  _______,    _______,  ES_BSLS, ES_SCLN, ES_COLN , ES_UNDS, _______,
+    //----------------------------------                                         -------------------------------------
+                                       _______, _______, _______, _______, _______, _______, _______, _______),
+
+
+  [_ADJUST] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                             _______, _______, _______, _______, _______,  _______, _______, _______
+  )
+  
+
     
 
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) { return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST); }
+layer_state_t layer_state_set_user(layer_state_t state) { return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST ); }
 
-// SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
-#ifdef OLED_DRIVER_ENABLE
 
-oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (!is_keyboard_master()) return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-    return rotation;
-}
-
-// When you add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void        set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
-
-// const char *read_mode_icon(bool swap);
-// const char *read_host_led_state(void);
-// void set_timelog(void);
-// const char *read_timelog(void);
-
-void oled_task_user(void) {
-    if (is_keyboard_master()) {
-        // If you want to change the display of OLED, you need to change here
-        oled_write_ln(read_layer_state(), false);
-        oled_write_ln(read_keylog(), false);
-        oled_write_ln(read_keylogs(), false);
-        // oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
-        // oled_write_ln(read_host_led_state(), false);
-        // oled_write_ln(read_timelog(), false);
-    } else {
-        oled_write(read_logo(), false);
-    }
-}
-#endif  // OLED_DRIVER_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
